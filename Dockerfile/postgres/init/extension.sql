c17d2c9419095951e355894ec15a2bf11a18b75a
usrtax <i@user.tax>
2022-10-19 21:55:32 +0800

    ♨

--- Dockerfile/postgres/init/extension.sql
+++ Dockerfile/postgres/init/extension.sql
@@ -1,3 +1,23 @@
-create extension uint;
-create extension md5hash;
+CREATE EXTENSION uint;
 
+CREATE EXTENSION md5hash;
+
+UPDATE
+  pg_type
+SET oid = 990001
+WHERE typname = 'u8';
+
+UPDATE
+  pg_type
+SET oid = 990002
+WHERE typname = 'u16';
+
+UPDATE
+  pg_type
+SET oid = 990003
+WHERE typname = 'u32';
+
+UPDATE
+  pg_type
+SET oid = 990004
+WHERE typname = 'u64';

ac0371f21c8cb2e748d4a14253822be7f6c93541
usrtax <i@user.tax>
2022-10-20 14:49:02 +0800

    ♨

--- api/run.sh
+++ api/run.sh
@@ -4,10 +4,10 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
-INIT='./init.mjs'
+INIT='./init.js'
+
 if [ -f "$INIT" ]; then
-$INIT
+  $INIT
 fi
 
-exec node --es-module-specifier-resolution=node --trace-uncaught --expose-gc --unhandled-rejections=strict ./api.mjs
-
+exec node --es-module-specifier-resolution=node --trace-uncaught --expose-gc --unhandled-rejections=strict ./api.js
